b13d1be62b6c256c3e9cb73b0b612bb58ab79e49
Yoga <yoga.herawan89@gmail.com>
2020-04-01 16:18:29 +0700

    ADD auto register account if validate success

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -156,188 +156,198 @@ UsersController.login = async(req, res, next) => {
         // check table ms_it_personal_data
         // if ZTIPE eq L (LDAP) then check userexistLDAP ? generate token
         // else not user LDAP then cek database table ms_it_personal_data and check password encrypt
-
-        let where       = [{ key: 'EMAIL', value: email }]
-        let users_tbl   = await UsersModel.getBy('PERNR, NAME, AD_USERNAME, EMAIL, ZTIPE, ZPASSWORD, ZROLE', where)
-
-        let token       = ''
-
-        if (users_tbl.EMAIL !== undefined) {
-            //check user via LDAP
-            const emailParam    = users_tbl.EMAIL.split('@')
-            const options       = {
-                method: 'POST',
-                url: partnersApi.ldapService.login,
-                body: {
-                    username: emailParam[0],
-                    password: password,
-                    method: 'login'
-                },
-                json: true,
-            }
-
-            const ldap = await rp(options)
-            
-            if (ldap != null) {
-                let validatorsRandom = randomstring.generate()
-                let userData         = [{ key: 'VALIDATOR', value : validatorsRandom }]
-                let condition        = [{ key: 'EMAIL', value: users_tbl.EMAIL }]
-
-                if (ldap.Status == '00') {
-                    //save validator random
-                    await UsersModel.save(userData, condition)
-                    let userObj = {
-                        pernr: users_tbl.PERNR,
-                        name: users_tbl.NAME,
-                        username: users_tbl.AD_USERNAME,
-                        email: users_tbl.EMAIL,
-                        tipe: users_tbl.ZTIPE,
-                        role: users_tbl.ZROLE,
-                        validator: validatorsRandom
-                    }
-                    token = await generateToken(userObj)
-
-                    
-                    
-                    result = {
-                        token:token,
-                        pernr: users_tbl.PERNR,
-                        name: users_tbl.NAME,
-                        username: users_tbl.AD_USERNAME,
-                        email: users_tbl.EMAIL,
-                        tipe: users_tbl.ZTIPE,
-                        role: users_tbl.ZROLE,
-                    }
-                } else {
-                    //check user from manual lookup table on database
-                    let options     = [
-                                        { key: 'EMAIL', value: email },
-                                        { key: 'ZPASSWORD', value: pwdEncrypt }
-                                    ]
-                    let userCheck   = await UsersModel.getBy('PERNR, NAME, AD_USERNAME, EMAIL, IS_ACTIVE, ZTIPE, ZPASSWORD, ZROLE', options)
-
-                    if (userCheck.EMAIL !== undefined) {
-                        if (userCheck.IS_ACTIVE !== 0) {                           
-                            //save validator random
-                            await UsersModel.save(userData, condition)
-                            // login success
-                            let userObj = {
-                                pernr: users_tbl.PERNR,
-                                name: users_tbl.NAME,
-                                username: users_tbl.AD_USERNAME,
-                                email: users_tbl.EMAIL,
-                                is_active: users_tbl.IS_ACTIVE,
-                                tipe: users_tbl.ZTIPE,
-                                role: users_tbl.ZROLE,
-                                validator: validatorsRandom
-                            }
+        const emailParam    = email.split('@')
         
-                            token = await generateToken(userObj)
+        if((emailParam.length > 1) && (emailParam[1] == "pertamina.com")){
+            let where       = [{ key: 'EMAIL', value: email }]
+            let users_tbl   = await UsersModel.getBy('PERNR, NAME, AD_USERNAME, EMAIL, ZTIPE, ZPASSWORD, ZROLE', where)
 
-                            result = {
-                                token: token,
-                                pernr: users_tbl.PERNR,
-                                name: users_tbl.NAME,
-                                username: users_tbl.AD_USERNAME,
-                                email: users_tbl.EMAIL,
-                                is_active: users_tbl.IS_ACTIVE,
-                                tipe: users_tbl.ZTIPE,
-                                role: users_tbl.ZROLE,
+            let token       = ''
+
+            if (users_tbl.EMAIL !== undefined) {
+                //check user via LDAP
+                const emailParam    = users_tbl.EMAIL.split('@')
+                const options       = {
+                    method: 'POST',
+                    url: partnersApi.ldapService.login,
+                    body: {
+                        username: emailParam[0],
+                        password: password,
+                        method: 'login'
+                    },
+                    json: true,
+                }
+
+                const ldap = await rp(options)
+                
+                if (ldap != null) {
+                    let validatorsRandom = randomstring.generate()
+                    let userData         = [{ key: 'VALIDATOR', value : validatorsRandom }]
+                    let condition        = [{ key: 'EMAIL', value: users_tbl.EMAIL }]
+
+                    if (ldap.Status == '00') {
+                        //save validator random
+                        await UsersModel.save(userData, condition)
+                        let userObj = {
+                            pernr: users_tbl.PERNR,
+                            name: users_tbl.NAME,
+                            username: users_tbl.AD_USERNAME,
+                            email: users_tbl.EMAIL,
+                            tipe: users_tbl.ZTIPE,
+                            role: users_tbl.ZROLE,
+                            validator: validatorsRandom
+                        }
+                        token = await generateToken(userObj)
+
+                        
+                        
+                        result = {
+                            token:token,
+                            pernr: users_tbl.PERNR,
+                            name: users_tbl.NAME,
+                            username: users_tbl.AD_USERNAME,
+                            email: users_tbl.EMAIL,
+                            tipe: users_tbl.ZTIPE,
+                            role: users_tbl.ZROLE,
+                        }
+                    } else {
+                        //check user from manual lookup table on database
+                        let options     = [
+                                            { key: 'EMAIL', value: email },
+                                            { key: 'ZPASSWORD', value: pwdEncrypt }
+                                        ]
+                        let userCheck   = await UsersModel.getBy('PERNR, NAME, AD_USERNAME, EMAIL, IS_ACTIVE, ZTIPE, ZPASSWORD, ZROLE', options)
+
+                        if (userCheck.EMAIL !== undefined) {
+                            if (userCheck.IS_ACTIVE !== 0) {                           
+                                //save validator random
+                                await UsersModel.save(userData, condition)
+                                // login success
+                                let userObj = {
+                                    pernr: users_tbl.PERNR,
+                                    name: users_tbl.NAME,
+                                    username: users_tbl.AD_USERNAME,
+                                    email: users_tbl.EMAIL,
+                                    is_active: users_tbl.IS_ACTIVE,
+                                    tipe: users_tbl.ZTIPE,
+                                    role: users_tbl.ZROLE,
+                                    validator: validatorsRandom
+                                }
+            
+                                token = await generateToken(userObj)
+
+                                result = {
+                                    token: token,
+                                    pernr: users_tbl.PERNR,
+                                    name: users_tbl.NAME,
+                                    username: users_tbl.AD_USERNAME,
+                                    email: users_tbl.EMAIL,
+                                    is_active: users_tbl.IS_ACTIVE,
+                                    tipe: users_tbl.ZTIPE,
+                                    role: users_tbl.ZROLE,
+                                }
+                            } else {
+                                // login not authorize
+                                statusCode      = 200
+                                responseCode    = '45'
+                                message         = 'Login Not Authorized, Account Is Nonactive'
+                                acknowledge     = false
+                                result          = null                            
                             }
                         } else {
                             // login not authorize
                             statusCode      = 200
-                            responseCode    = '45'
-                            message         = 'Login Not Authorized, Account Is Nonactive'
+                            responseCode    = '05'
+                            message         = 'Login Not Authorized, Password Incorrect'
                             acknowledge     = false
-                            result          = null                            
+                            result          = null
                         }
-                    } else {
-                        // login not authorize
-                        statusCode      = 200
-                        responseCode    = '05'
-                        message         = 'Login Not Authorized, Password Incorrect'
-                        acknowledge     = false
-                        result          = null
+                    }
+                } else {
+                    // return LDAP Service null
+                    statusCode      = 200
+                    responseCode    = '99'
+                    message         = 'Error return response LDAP Service'
+                    acknowledge     = false
+                    result          = null
+                }
+            } else if (users_tbl.EMAIL == undefined) {
+                //user tidak terdaftar di email dan LDAP jadi perlu di create ke DB berdasarkan hasil LDAP
+                const emailParam    = email.split('@')
+                const options       = {
+                    method: 'POST',
+                    url: partnersApi.ldapService.login,
+                    body: {
+                        username: emailParam[0],
+                        password: password,
+                        method: 'validate'
+                    },
+                    json: true,
+                }
+
+                const ldap = await rp(options)
+                
+                if (ldap != null) {
+                    //start proses register pekerja
+                    if (ldap.Status == '00') {
+                        let validatorsRandom = randomstring.generate()
+                        let roleDefault = "3" //Rakyat Jelata
+                        let typeLDAP = "L"
+                    
+                        let data = [
+                            {key : 'PERNR', value : ldap.Data.EmpNumber},
+                            {key : 'NAME', value : ldap.Data.NamaLengkap},
+                            {key : 'AD_USERNAME', value : ldap.Data.Email},
+                            {key : 'EMAIL', value : ldap.Data.Email},
+                            {key : 'ASSIGNMENT_NUMBER', value : ldap.Data.EmpNumber},
+                            {key : 'COCODE', value : "2110"},
+                            {key : 'IS_ACTIVE', value : "1"},
+                            {key : 'ZTIPE', value : typeLDAP},
+                            {key : 'ZROLE', value : roleDefault},
+                            { key: 'VALIDATOR', value : validatorsRandom }
+                        ]
+                        
+                        //save validator random
+                        await UsersModel.save(data)
+                        let userObj = {
+                            pernr: ldap.Data.EmpNumber,
+                            name: ldap.Data.NamaLengkap,
+                            username: ldap.Data.Email,
+                            email: ldap.Data.Email,
+                            tipe: typeLDAP,
+                            role: roleDefault,
+                            validator: validatorsRandom
+                        }
+                        token = await generateToken(userObj)
+
+                        result = {
+                            token:token,
+                            pernr: ldap.Data.EmpNumber,
+                            name: ldap.Data.NamaLengkap,
+                            username: ldap.Data.Email,
+                            email: ldap.Data.Email,
+                            tipe: typeLDAP,
+                            role: roleDefault,
+                        }
                     }
                 }
             } else {
-                // return LDAP Service null
+                // login not authorize
                 statusCode      = 200
-                responseCode    = '99'
-                message         = 'Error return response LDAP Service'
+                responseCode    = '55'
+                message         = 'Login Not Authorized, User not exist'
                 acknowledge     = false
                 result          = null
             }
-        } else if (users_tbl.EMAIL == undefined) {
-            //user tidak terdaftar di email dan LDAP jadi perlu di create ke DB berdasarkan hasil LDAP
-            const emailParam    = email.split('@')
-            const options       = {
-                method: 'POST',
-                url: partnersApi.ldapService.login,
-                body: {
-                    username: emailParam[0],
-                    password: password,
-                    method: 'validate'
-                },
-                json: true,
-            }
 
-            const ldap = await rp(options)
-            
-            if (ldap != null) {
-                //start proses register pekerja
-                if (ldap.Status == '00') {
-                    let validatorsRandom = randomstring.generate()
-                    let roleDefault = "3" //Rakyat Jelata
-                    let typeLDAP = "L"
-                
-                    let data = [
-                        {key : 'PERNR', value : ldap.Data.EmpNumber},
-                        {key : 'NAME', value : ldap.Data.NamaLengkap},
-                        {key : 'AD_USERNAME', value : ldap.Data.Email},
-                        {key : 'EMAIL', value : ldap.Data.Email},
-                        {key : 'ASSIGNMENT_NUMBER', value : ldap.Data.EmpNumber},
-                        {key : 'COCODE', value : "2110"},
-                        {key : 'IS_ACTIVE', value : "1"},
-                        {key : 'ZTIPE', value : typeLDAP},
-                        {key : 'ZROLE', value : roleDefault},
-                        { key: 'VALIDATOR', value : validatorsRandom }
-                    ]
-                    console.log(data)
-                    //save validator random
-                    await UsersModel.save(data)
-                    let userObj = {
-                        pernr: ldap.Data.EmpNumber,
-                        name: ldap.Data.NamaLengkap,
-                        username: ldap.Data.Email,
-                        email: ldap.Data.Email,
-                        tipe: typeLDAP,
-                        role: roleDefault,
-                        validator: validatorsRandom
-                    }
-                    token = await generateToken(userObj)
 
-                    result = {
-                        token:token,
-                        pernr: ldap.Data.EmpNumber,
-                        name: ldap.Data.NamaLengkap,
-                        username: ldap.Data.Email,
-                        email: ldap.Data.Email,
-                        tipe: typeLDAP,
-                        role: roleDefault,
-                    }
-                }
-            }
         } else {
-            // login not authorize
-            statusCode      = 200
-            responseCode    = '55'
-            message         = 'Login Not Authorized, User not exist'
-            acknowledge     = false
-            result          = null
+            result = ""
+            responseCode = "05"
+            message = "Email kurang lengkap. Harus dengan @pertamina.com"
         }
 
+        
         // return response
         res.status(statusCode).send(
             parseResponse(acknowledge, result, responseCode, message)
